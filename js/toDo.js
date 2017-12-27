88fb47555407447b37d0b757a31704388c056507
Dreg0 <andrii.kruglakov@gmail.com>
2017-12-27 19:00:09 +0200

    all done exept storing the data

--- js/toDo.js
+++ js/toDo.js
@@ -3,26 +3,53 @@ let removeSvg = '<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/s
 let completeSvg = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 22 22" xml:space="preserve"> <rect y="0" class="noFill" width="22" height="22"/> <g> <path class="fill" d="M9.7,14.4L9.7,14.4c-0.2,0-0.4-0.1-0.5-0.2l-2.7-2.7c-0.3-0.3-0.3-0.8,0-1.1s0.8-0.3,1.1,0l2.1,2.1l4.8-4.8c0.3-0.3,0.8-0.3,1.1,0s0.3,0.8,0,1.1l-5.3,5.3C10.1,14.3,9.9,14.4,9.7,14.4z"/> </g> </svg>'
 
 
-// window.onload = function(){} in case of moving js in html from body
-// function buttonClick(){
-// }
+
+// let data= {
+//  todo =[],
+//  completed = []
+// };
 
 
 // User clicked on the add button
 // If there is any text inside the item field, add that text to the todo list
 document.getElementById('add-activity').addEventListener('click', function(){
     let value = document.getElementById('toDo_item').value;
-    if (value) addItemTodo (value);
+    if (value) {
+        // data.todo.push(value);
+        addItemTodo (value);
+        document.getElementById('toDo_item').value ='';
+    };
 });
 
+//Pressing enter after adding text in form
+document.getElementById('toDo_item').addEventListener('keypress', function (e) {
+    var key = e.which || e.keyCode;
+    let value = document.getElementById('toDo_item').value;
+    if (key === 13) { // 13 is enter
+      // code for enter
+      addItemTodo (value);
+        document.getElementById('toDo_item').value ='';
+    }
+});
 
+function removeItem(){
+    let item = this.parentNode.parentNode;
+    let parent =item.parentNode;
+    parent.removeChild(item);
+}
+
+function completeItem(){
+    let item = this.parentNode.parentNode;
+    let parent = item.parentNode;
+    let id = parent.id;
+    let target = (id === 'todo') ? document.getElementById('completed-tasks'):document.getElementById('todo')
+    parent.appendChild(item);
+    target.insertBefore(item, target.childNodes[0])
+}
 
 //adding new items into to do list
-
 function addItemTodo(text){
     let list = document.getElementById('todo');
-
-
 
     let item = document.createElement('li');
     item.classList.add('toDo-list-tasks-items');
@@ -36,18 +63,21 @@ function addItemTodo(text){
     remove.classList.add('remove-task');
     remove.innerHTML =removeSvg
 
+    //add click event for removing task
+    remove.addEventListener('click',removeItem);
+    
     let add = document.createElement('button');
     add.classList.add('toDo-list-tasks-button');
     add.classList.add('complete-task');
     add.innerHTML = completeSvg
+    
+    //add click event for completing task
+    add.addEventListener('click',completeItem);
 
     divButtons.appendChild(remove);
     divButtons.appendChild(add);
-
     item.appendChild(divButtons);
-
-    list.appendChild(item);
-
+    list.insertBefore(item, list.childNodes[0]);
 }
 
 
